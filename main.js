01a0c42a2048e4bed31bb22d3bf9b9da21362aa6
stehfany <sthefanyclarismundo23@gmail.com>
2022-08-22 09:15:32 -0300

    funcao

---
+++ main.js
@@ -0,0 +1,19 @@
+
+  function validar() {
+var inscricao = form.email;
+
+  if (inscricao.value == " "){
+    alert("Digite um email valido!");
+    inscricao.focus();
+    return false;
+  }else{
+    alert("Recebemos seu email com sucesso!")
+  }
+  
+  }
+      
+
+    // function cadastro(){
+    //     alert('Recebemos seu email com sucesso!');
+    //   }
+    
